26e505cab44784ff8e27fbbd4957290ee65fc1b2
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-23 11:26:09 -0300

    feat: add countdown method

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
 
 import ExperienceBar from "../components/ExperienceBar";
 import Profile from '../components/Profile';
-import CompleteChallenges from '../components/CompleteChallenges'
+import CompleteChallenges from '../components/CompleteChallenges';
+import CountDown from '../components/Countdown'
+
+
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title> Move it</title>
+      </Head>
     <ExperienceBar/>
     <section >
       <div>
         <Profile/>
         <CompleteChallenges/>
+        <CountDown/>
       </div>
       <div>
       </div>
